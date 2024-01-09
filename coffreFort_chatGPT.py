50e54f38532d1cbe2312c0c5e1f608034ddb15ac
matheoflln <156067253+matheoflln@users.noreply.github.com>
2024-01-09 15:46:56 +0100

    Add files via upload

---
+++ coffreFort_chatGPT.py
@@ -0,0 +1,17 @@
+'''Coffre fort
+Trouver le code à 4 chiffres du coffre-fort
+'''
+def code_bon(m,d,c,u):
+    rep = m%2==0 and m+c==15 and d==c-m and m==u*d
+    return rep
+
+def decomposer(nb):
+    u=nb%10
+    d=int(nb/10)%10
+    c=int(nb/100)%10
+    m=int(nb/100)%10
+    return(m,c,d,u)
+
+
+
+
